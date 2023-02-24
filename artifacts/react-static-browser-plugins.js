952f758815837f6222990938cc3b6926d762212e
Himanshu Mamodiya <himanshu.mamodiya@nagarro.com>
2023-02-24 16:10:56 +0530

    first commit

---
+++ artifacts/react-static-browser-plugins.js
@@ -0,0 +1,17 @@
+// Imports
+
+
+// Plugins
+const plugins = [{
+  location: "D:/Firebase/time-calc/node_modules/react-static-plugin-jss",
+  plugins: [],
+  hooks: {}
+},
+{
+  location: "D:/Firebase/time-calc",
+  plugins: [],
+  hooks: {}
+}]
+
+// Export em!
+export default plugins
